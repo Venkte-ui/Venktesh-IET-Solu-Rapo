d42c46de531b0e92d28423f67515f66265245e4a
VENKTESH KUMAR MISRA <venkteshkumarmisra8181@gmail.com>
2023-02-03 19:58:12 +0530

    Created server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+const port=8000;
+app.listen(port,function(err)
+{ 
+    gif(err)
+    {
+    console.log('Error');
+    }
+    else{
+        console.log("Error is not present");
+    }
+})
